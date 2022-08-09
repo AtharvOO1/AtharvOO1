3a60dea722b42f8d26064371abeb4dd4398b9648
AtharvOO1 <105447880+AtharvOO1@users.noreply.github.com>
2022-08-09 18:50:04 +0530

    Add files via upload

---
+++ Atharv Dixit Assignment3.py
@@ -0,0 +1,45 @@
+#1. Why are functions advantageous to have in your programs?
+#2. When does the code in a function run: when it's specified or when it's called?
+#3. What statement creates a function?
+#4. What is the difference between a function and a function call?
+#5. How many global scopes are there in a Python program? How many local scopes?
+#6. What happens to variables in a local scope when the function call returns?
+#7. What is the concept of a return value? Is it possible to have a return value in an expression?
+#8. If a function does not have a return statement, what is the return value of a call to that function?
+#9. How do you make a function variable refer to the global variable?
+#10. What is the data type of None?
+#11. What does the sentence import areallyourpetsnamederic do?
+#12. If you had a bacon() feature in a spam module, what would you call it after importing spam?
+#13. What can you do to save a programme from crashing if it encounters an error?
+#14. What is the purpose of the try clause? What is the purpose of the except clause?
+
+
+#Ans1: Functions are advantageous in our program as it reduces the chances of error and also reduces the need for duplicate code. Modifying a program also becomes easier.
+
+#Ans2: The code in a function run when it's called.
+
+#Ans3: A function is created with the def keyword. The def keyword is followed by the function name with round brackets and a colon.
+#      For eg: def product(a,b):
+
+#Ans4: A function is a block of code that has statements and does a particular operation.
+#      A function call is used to execute the function whenever is required.
+
+#Ans5: There’s only one global Python scope per program execution.
+
+#Ans6: The variable in local scope destroys.
+
+#Ans7: The return value gives the value with its type. Yes, it can be used in expression.
+
+#Ans8: The return value of the called function is undefined and gives nothing.
+
+#Ans9: Global keywords can be used to make a function variable refer to the global variable.
+
+#Ans10: The data type of None is NoneType
+
+#Ans11: It imports the module named areallyourpetsnamederic.
+
+#Ans12: It can be called with spam.bacon()
+
+#Ans13: We can use exception handeling and use try & except in our program.
+
+#Ans14: The try block test a block of code for errors while the except block handles the error.
